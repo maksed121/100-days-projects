9c85b2e942274da37712598b726f0635edc3ac2a
maksed121 <maksedali0013@gmail.com>
2025-09-04 22:43:01 +0530

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,4 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
@@ -21,11 +12,12 @@ const eslintConfig = [
     ],
   },
   {
-    plugins: ["react-hooks"],
-    // ...
+    plugins: {
+      "react-hooks": reactHooks,
+    },
     rules: {
       "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn", // <--- THIS IS THE NEW RULE
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
